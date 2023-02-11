030ee96bf3e963944ae2b645a7c26c4bcb365fb3
balasivarathri <77465585+balasivarathri@users.noreply.github.com>
2023-02-11 16:08:32 +0200

    api second commit

--- src/main/java/org/api/resetassured/AddPlaceAPI.java
+++ src/main/java/org/api/resetassured/AddPlaceAPI.java
@@ -2,11 +2,13 @@ package org.api.resetassured;
 
 import io.restassured.RestAssured;
 import io.restassured.path.json.JsonPath;
+import static org.hamcrest.Matcher.*;
 
 import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.equalTo;
 
 public class AddPlaceAPI {
-    public static <JsonArray, JsonObject> void main(String[] args) {
+    public static void main(String[] args) {
         //validate if AddPlaceAPI is working as expected
         //given ---> all input details
         //when ---> submit the api resource and http method
@@ -20,10 +22,22 @@ public class AddPlaceAPI {
                 when().post("maps/api/place/add/json").
                 then().statusCode(200).extract().body().asPrettyString();
         System.out.println(addApiResponse);
-
+        String placeId= ReUsableMethods.rawToJson(addApiResponse).getString("place_id");
+        System.out.println(placeId);
         //Update place API
-
+        String updateApiResponse = given().queryParam("key","qaclick123").body("{\n" +
+                "\"place_id\":\""+placeId+"\",\n" +
+                "\"address\":\"70 Summer walk, USA\",\n" +
+                "\"key\":\"qaclick123\"\n" +
+                "}\n").header("Content-Type","application/json").
+                when().put("maps/api/place/update/json").
+                then().assertThat().body("msg",equalTo("Address successfully updated")).extract().response().asPrettyString();
+        System.out.println(updateApiResponse);
 
         //Get place API
+        String getApiResponse = given().queryParam("key","qaclick123").queryParam("place_id",placeId).
+                when().get("maps/api/place/get/json").
+                then().assertThat().body("address",equalTo("70 Summer walk, USA")).extract().response().asString();
+        System.out.println(getApiResponse);
     }
 }
