e73dc15958d3c29ad5404d6f8515446c69c5f8c4
balasivarathri <77465585+balasivarathri@users.noreply.github.com>
2023-02-11 20:10:21 +0200

    api second commit

--- src/main/java/org/api/resetassured/ComplexJsonParse.java
+++ src/main/java/org/api/resetassured/ComplexJsonParse.java
@@ -1,4 +1,30 @@
 package org.api.resetassured;
 
-public class ComplexJson {
+import io.restassured.path.json.JsonPath;
+
+public class ComplexJsonParse {
+
+    public static void main(String[] args) {
+        JsonPath js = new JsonPath(Payloads.coursePrice());
+        //Print No of courses returned by API
+        int count = js.getInt("courses.size()");
+        System.out.println(count);
+
+        //Print Purchase Amount and website
+        int purchaseAmount = js.getInt("dashboard.purchaseAmount");
+        System.out.println(purchaseAmount);
+        String website = js.getString("dashboard.website");
+        System.out.println(website);
+
+        //Print Title of the first course
+        String title = js.get("courses[2].title");
+        System.out.println(title);
+
+        //Print All course titles and their respective Prices
+        for(int i=0;i<count;i++){
+            String courseTitles = js.get("courses["+i+"].title");
+            System.out.println(courseTitles);
+            System.out.println(js.get("courses["+i+"].price").toString());
+        }
+    }
 }
